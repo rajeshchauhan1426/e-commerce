807e23a852fa366ff6e7a3f83ca6bef31582ab18
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-12-20 00:18:16 +0530

    image of user update

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -12,7 +12,7 @@ const Avatar: React.FC<AvatarProps> = ({ src }) => {
       height={30} // Use a numeric value instead of a string
       width={30}  // Use a numeric value instead of a string
       alt='User Avatar'
-      src={src || '/images/user.png'} // Default to user.png if src is null or undefined
+      src={src || '/images/user2.png'} // Default to user.png if src is null or undefined
     />
   )
 }
